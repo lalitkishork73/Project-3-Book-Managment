3458f727f01d0802cfc06887fe18cadfcc01bfe0
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-11-30 19:20:38 +0530

    added

---
+++ client/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink,Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Navbar
