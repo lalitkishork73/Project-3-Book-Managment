c957c68068ee672a0af20a45ff4898f6541ecaf5
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-02-01 17:41:18 +0530

    updated

--- src/components/homecontent.jsx
+++ src/components/homecontent.jsx
@@ -44,9 +44,9 @@ const Homecontent = () => {
                     <div className='p-10 grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-3  bg-[#fdfdfd80] w-[100%] md:w-[65%]' >
 
                         {
-                            list.map((item, id) => (
+                            list.map((item, index) => (
                                 <>
-                                    <div key={id + 1} className='flex flex-col drop-shadow-xl rounded-xl bg-[#fdfdfd] backdrop-blur-sm h-auto hover:bg-[#edf5fc] w-auto p-1'>
+                                    <div key={index} className='flex flex-col drop-shadow-xl rounded-xl bg-[#fdfdfd] backdrop-blur-sm h-auto hover:bg-[#edf5fc] w-auto p-1'>
                                         <div className='rounded-xl flex
             justify-center w-auto bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 to-gray-600 bg-gradient-to-r'>
                                             <img className='rounded-xl min-w-[10rem] max-h-[15rem]' src={!item?.bookCover ? poster : item.bookCover} />
