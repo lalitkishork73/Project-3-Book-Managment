d4c255c65e8b37a81dd096c294f33653bae9f9f0
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-02-07 18:29:02 +0530

    added

--- src/components/homecontent.jsx
+++ src/components/homecontent.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom';
 import axios from 'axios';
-import poster from '../assets/poster.png'
+import { BiSearchAlt2 } from 'react-icons/bi'
+import CardHome from './CardHome'
 
 
 const Homecontent = () => {
     const [list, setList] = useState([]);
     const [errr, setErr] = useState('');
+    const [search, setSearch] = useState('');
+    const [searchData, setsearchData] = useState([]);
 
     useEffect(() => {
         const getData = async () => {
@@ -34,39 +36,40 @@ const Homecontent = () => {
 
     return (
         <>
+            <div className=' flex justify-center bg-black p-7 '>
+                <input type="text" placeholder="Search" className='pl-2 p-2 w-[100%] sm:w-[30%] bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:sky-500 block rounded-l-md sm:text-sm'
+                />
+                <button className='text-white text-3xl bg-slate-800 rounded-r-md pl-2 pr-2 p-2'><BiSearchAlt2 /></button>
+            </div>
             <div className='flex justify-center bg-gradient-to-r from-blue-400 to-emerald-400'>
                 {errr ? <>
                     <div className='h-[50vh] flex justify-center items-center'>
                         <h1 className='text-white text-4xl text-center'>Book Not Found!</h1>
                     </div>
-                </> : <>
-                    <div className='p-10 grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-3  bg-[#fdfdfd80] w-[100%] md:w-[65%]' >
-
-                        {
-                            list.map((item, index) => (
+                </> : !search ? <>
+                    {
+                        searchData ? <>
+                            {searchData.map((item, index) => (
                                 <>
-                                    <div key={index} className='flex flex-col drop-shadow-xl rounded-xl bg-[#fdfdfd] backdrop-blur-sm h-auto hover:bg-[#edf5fc] w-auto p-1'>
-                                        <div className='rounded-xl flex
-            justify-center w-auto bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 to-gray-600 bg-gradient-to-r'>
-                                            <img className='rounded-xl min-w-[10rem] max-h-[15rem]' src={!item?.bookCover ? poster : item.bookCover} />
-                                        </div>
-                                        <div className='flex flex-col text-center md:text-justify p-5 w-auto text-sm'>
-                                            <h1 className='font-bold mb-2 uppercase'>{item.title}</h1>
-                                            <h3 className='text-xs text-gray-500'><span className='bg-cyan-400 text-white p-1 rounded-lg '> Catagory </span> &nbsp; {item.category}</h3>
-                                            <p className='p-1 h-20 overflow-hidden'>{item.excerpt}</p>
-                                            <p className='p-1'><span className='font-bold'>ISBN -</span>&nbsp; {item.ISBN}</p>
-                                            <p className=''><span className='bg-black text-white p-1 rounded-md text-xs'>Total Reviews </span> &nbsp; {item.reviews
-                                            }</p>
-                                            <NavLink to={`/review/${item._id}`} >
-                                                <button className='bg-gradient-to-r from-green-400 to-blue-300 hover:from-pink-200 hover:to-yellow-500 p-1 rounded-lg w-16 mt-2 text-xs'>Review</button>
-                                            </NavLink>
-                                        </div>
-                                    </div>
+                                    <CardHome item={item} index={index} />
                                 </>
-                            ))
-                        }
-                    </div >
-                </>}
+                            ))}
+                        </> : <><div><h1>data not Found</h1></div></>
+                    }
+                </> :
+                    <>
+                        <div className='p-10 grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-3  bg-[#fdfdfd80] w-[100%] md:w-[65%]' >
+
+                            {
+                                list.map((item, index) => (
+                                    <>
+                                        <CardHome item={item} index={index} />
+                                    </>
+                                ))
+                            }
+                        </div >
+                    </>
+                }
             </div>
         </>
     )
