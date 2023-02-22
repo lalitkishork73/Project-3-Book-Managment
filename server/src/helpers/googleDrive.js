b18351a9d4d8b2d7d29c97cbfca238c4acddba06
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-02-22 20:36:02 +0530

    updated few things

--- server/src/helpers/googleDrive.js
+++ server/src/helpers/googleDrive.js
@@ -4,11 +4,11 @@ const { OAuth2Client } = require('google-auth-library');
 const path = require('path');
 // const fs = require('fs')
 const GOOGLE_API_FOLDER_ID = process.env.GOOGLE_API_FOLDER_ID;
-const CLIENT_ID=process.env.CLIENT_ID
-const CLIENT_SECRET=process.env.CLIENT_SECRET
-const ACCESS_TOKEN=process.env.ACCESS_TOKEN
-const REFRESH_TOKEN=process.env.REFRESH_TOKEN
-const EXPIRY_TIME="1y"
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN
+const EXPIRY_TIME = 15780000
 
 // there lots of way to implement google drive setup only you have to decide which method do you required.
 
@@ -30,12 +30,15 @@ const uploadFiles = async (file) => {
             refresh_token: REFRESH_TOKEN,
             scopes: ['https://www.googleapis.com/auth/drive'],
             expiry_date: EXPIRY_TIME,
-          });
+        });
+
+        console.log(auth)
 
         const driveService = google.drive({
             version: 'v3',
             auth
         });
+
 
         const fileMetaData = {
             'name': file.originalname,
