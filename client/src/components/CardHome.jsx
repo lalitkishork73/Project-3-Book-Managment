67bd1f89e059b9c018db11121260f5b4434af629
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-02-22 21:10:35 +0530

    updated

--- client/src/components/CardHome.jsx
+++ client/src/components/CardHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import poster from '../assets/poster.png';  
+import poster from '../assets/poster.png';
 
 const CardHome = ({ item, index }) => {
     return (
@@ -8,7 +8,7 @@ const CardHome = ({ item, index }) => {
             <div key={index} className='flex flex-col drop-shadow-xl rounded-xl bg-[#fdfdfd] backdrop-blur-sm h-auto hover:bg-[#edf5fc] w-auto p-1'>
                 <div className='rounded-xl flex
             justify-center w-auto bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 to-gray-600 bg-gradient-to-r'>
-                    <img className='rounded-xl min-w-[10rem] max-h-[15rem]' src={!item?.bookCover ? poster : item.bookCover} />
+                    <img className='rounded-xl min-w-[10rem] max-h-[15rem]' src={item.bookCover} />
                 </div>
                 <div className='flex flex-col text-center md:text-justify p-5 w-auto text-sm'>
                     <h1 className='font-bold mb-2 uppercase'>{item.title}</h1>
